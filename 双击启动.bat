d9903443b056397fb73e1ce84bfaeeae031de10e
YaNgYu <75065276+YaN9Yu96@users.noreply.github.com>
2024-01-26 13:49:25 +0800

    Update 双击启动.bat

--- 双击启动.bat
+++ 双击启动.bat
@@ -1,18 +1,18 @@
 @echo off
 chcp 65001 > nul
 
-set venvDir=venv
-set scriptPath=flight.py
-
-if not exist %venvDir%\Scripts\activate (
-    echo 正在创建虚拟环境...请稍后...
-    python -m venv %venvDir%
-    
+IF NOT EXIST .\venv (
+    echo 正在创建虚拟环境...
+    python -m venv venv
+    call .\venv\Scripts\activate
     echo 正在安装依赖项...
-    call %venvDir%\Scripts\activate
+    pip install -r requirements.txt
+) else (
+    echo 加载虚拟环境...
+    call .\venv\Scripts\activate
+    echo 确认依赖项...
     pip install -r requirements.txt
 )
 
-call %venvDir%\Scripts\activate
-python %scriptPath%
+python flight.py
 deactivate
