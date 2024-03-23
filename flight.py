b0dea8261d1fb455799dddd3233ead86c9e089fd
YaNgYu <75065276+YaN9Yu96@users.noreply.github.com>
2024-03-23 21:54:32 +0800

    添加GUI

--- flight.py
+++ flight.py
@@ -3,74 +3,70 @@ import time
 import configparser
 import pygame
 import random
+import threading
 from plyer import notification
 from lxml import etree
-from colorama import init, Fore, Style
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 from mylib.utils import get_url
 
-# 初始化colorama
-init()
 # 变量
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 CONFIG_FILENAME = "config.ini"
 INTERVAL_RANDOM_MAX = 1
-# 说明
-PROMPT_FROM_AIRPORT = f"{Fore.GREEN}1.输入起飞机场IATA代码（例：SZX表示深圳宝安机场）{Style.RESET_ALL}"
-PROMPT_TO_AIRPORT = f"{Fore.GREEN}2.输入降落机场IATA代码{Style.RESET_ALL}"
-PROMPT_DATAS = f"{Fore.GREEN}3.输入飞行日期 (格式yyyy-mm-dd，多个日期用小写逗号,分隔){Style.RESET_ALL}"
-PROMPT_FLIGHTS = f"{Fore.GREEN}4.需要查询的航班号 (留空时表示监测全部，多个航班用小写逗号,分隔){Style.RESET_ALL}"
-PROMPT_PRICE = f"{Fore.GREEN}5.低于多少票价时进行提醒{Style.RESET_ALL}"
-PROMPT_INTERVAL = f"{Fore.GREEN}6.每次刷新间隔多长时间/秒 (过短的刷新间隔可能会被票务网站屏蔽){Style.RESET_ALL}"
+
+# 创建ConfigParser对象
+config = configparser.ConfigParser()
+config.read(CONFIG_FILENAME)
 
 
 class Checker:
     def __init__(self):
-        # 创建ConfigParser对象
-        self.config = configparser.ConfigParser()
-        self.config.read(CONFIG_FILENAME)
+        pass
 
+    def run(self):
         # 使用之前的配置或询问用户输入
-        self.from_airport = self.get_or_input("from_airport", PROMPT_FROM_AIRPORT)
-        self.to_airport = self.get_or_input("to_airport", PROMPT_TO_AIRPORT)
-        self.dates = self.get_or_input("datas", PROMPT_DATAS).split(",")
-        self.flights = self.get_or_input("flights", PROMPT_FLIGHTS).split(",")
-        self.price = int(self.get_or_input("price", PROMPT_PRICE))
-        self.interval = float(self.get_or_input("interval", PROMPT_INTERVAL))
-
+        self.from_airport = self.get_or_input("from_airport", "起飞机场IATA代码")
+        self.to_airport = self.get_or_input("to_airport", "降落机场IATA代码")
+        self.dates = self.get_or_input("datas", "飞行日期","（格式yyyy-mm-dd，多个日期用逗号分隔）").split(",")
+        self.flights = self.get_or_input("flights", "需要查询的航班号","（留空时表示监测全部，多个航班用逗号分隔）").split(",")
+        self.price = int(self.get_or_input("price", "监测票价","（当票价低于该设定时进行提醒）"))
+        self.interval = float(self.get_or_input("interval", "监测间隔/秒","（每轮监测的间隔，过短的时间可能会被网站屏蔽导致监测失效）"))
         print("已完成配置，开始监测...")
         print(f"如有需要, 可确认以下网址是否正常加载, 避免配置有误:")
         for date in self.dates:
             print(self.get_airfare_url(date))
 
-    def get_or_input(self, key, prompt):
+    def get_or_input(self, key, title, prompt=""):
         section = "Settings"
-        if section not in self.config:
-            self.config[section] = {}
+        if section not in config:
+            config[section] = {}
 
-        def modify_value(prompt):
-            value = input(prompt).strip()
-            self.config.set(section, key, value)
+        def modify_value(prompt, current_value=""):
+            new_value = simpledialog.askstring("配置", prompt,initialvalue=current_value)
+            config.set(section, key, new_value)
             with open(CONFIG_FILENAME, "w") as configfile:
-                self.config.write(configfile)
-            return value
+                config.write(configfile)
+            return new_value
 
         # 从配置文件中获取值，如果不存在或用户需要修改则让用户输入
-        if self.config.has_option(section, key):
-            value = self.config.get(section, key)
-            modify = input(
-                f"{prompt}\n{Fore.GREEN} - 当前设置为: {value} 是否修改? (y:需要修改){Style.RESET_ALL}\n"
-            )
-            if modify.strip() == "y":
-                value = modify_value(f"输入后按回车键确认:")
+        if config.has_option(section, key):
+            value = config.get(section, key)
+            modify = messagebox.askyesno("确认", f"当前[{title}]为：[{value}]\n是否修改?")
+            if modify:
+                value = modify_value(f"请输入[{title}]：\n\n{prompt}", value)
             return value
         else:
-            value = modify_value(f"{prompt}\n输入后按回车键确认:")
+            value = modify_value(f"请输入[{title}]：\n\n{prompt}")
             return value
-
+        
     def get_airfare_url(self, date):
         return f"https://www.ly.com/flights/itinerary/oneway/{self.from_airport}-{self.to_airport}?date={date}"
 
     def check_tongcheng(self):
+        # 记录监测到的航班数量
+        flight_counts = {}
+
         for date in self.dates:
             url = self.get_airfare_url(date)
             response = get_url(url)
@@ -80,30 +76,15 @@ class Checker:
 
             html = etree.HTML(response.text)
             contents = html.xpath('//div[@class="flight-item"]')
+            flight_counts[date] = len(contents)
             for content in contents:
                 try:
-                    flight_name = content.xpath(
-                        './/p[@class="flight-item-name" and @data-v-55ab452e]/text()'
-                    )[0]
-                    price = int(
-                        content.xpath(
-                            './/div[@class="head-prices" and @data-v-55ab452e]//strong/em/text()'
-                        )[0][
-                            1:
-                        ]  # 去掉第一个币种符号
-                    )
-                    start_time = content.xpath(
-                        './/div[@class="f-startTime f-times-con" and @data-v-55ab452e]/strong/text()'
-                    )[0]
-                    start_airport = content.xpath(
-                        './/div[@class="f-startTime f-times-con" and @data-v-55ab452e]/em/text()'
-                    )[0]
-                    end_time = content.xpath(
-                        './/div[@class="f-endTime f-times-con" and @data-v-55ab452e]/strong/text()'
-                    )[0]
-                    end_airport = content.xpath(
-                        './/div[@class="f-endTime f-times-con" and @data-v-55ab452e]/em/text()'
-                    )[0]
+                    flight_name = content.xpath('.//p[@class="flight-item-name" and @data-v-55ab452e]/text()')[0]
+                    price = int(content.xpath('.//div[@class="head-prices" and @data-v-55ab452e]//strong/em/text()')[0][1:])
+                    start_time = content.xpath('.//div[@class="f-startTime f-times-con" and @data-v-55ab452e]/strong/text()')[0]
+                    start_airport = content.xpath('.//div[@class="f-startTime f-times-con" and @data-v-55ab452e]/em/text()')[0]
+                    end_time = content.xpath('.//div[@class="f-endTime f-times-con" and @data-v-55ab452e]/strong/text()')[0]
+                    end_airport = content.xpath('.//div[@class="f-endTime f-times-con" and @data-v-55ab452e]/em/text()')[0]
                 except Exception as e:
                     print(f"错误：{e}")
 
@@ -117,12 +98,13 @@ class Checker:
                         if price < self.price:
                             self.on_target_price(content)
             time.sleep(random.uniform(0.5, 1.5))
+        return flight_counts
 
     def on_target_price(self, content):
         self.play_sound()
         self.write_history(content)
         self.show_notification("机票监测", f"航班的票价已低于设定值！{content}")
-        print(f"{Fore.RED}航班的票价已低于设定值！{Style.RESET_ALL}")
+        print(f"航班的票价已低于设定值！")
 
     def play_sound(self):
         pygame.mixer.init()
@@ -148,14 +130,73 @@ class Checker:
             # 写入时间戳到文件
             file.write(f"{timestamp} {content}")
 
+def stop_monitoring():
+    running_label.config(text="监测已停止")
+    global monitoring
+    monitoring = False
 
-if __name__ == "__main__":
-    checker = Checker()
+def start_monitoring():
+    checker.run()
+    running_label.config(text="正在监测…")
+    global monitoring
+    monitoring = True
+    thread = threading.Thread(target=check_flights)
+    thread.start()
 
+def check_flights():
     try_count = 0
-
-    while True:
-        time.sleep(checker.interval + random.uniform(0, INTERVAL_RANDOM_MAX))
+    while monitoring:
         try_count += 1
+        flight_counts = checker.check_tongcheng()
+
         print(f"第{try_count}次刷新")
-        checker.check_tongcheng()
+        rounds_label.config(text=f"已经经过的轮次：{try_count}")
+
+        flight_count_text = "监测到的航班数量"
+        for date, count in flight_counts.items():
+            flight_count_text += f"\n{date}：{count}个班次"
+        flight_count_label.config(text=flight_count_text)
+
+        time.sleep(random.uniform(0.5, 1.5))
+
+def on_closing():
+    stop_monitoring()
+    stop_event.set()
+    root.destroy()
+
+def main_thread():
+    while not stop_event.is_set():
+        time.sleep(1)
+
+# 初始化Tkinter
+monitoring = False
+checker = Checker()
+
+root = tk.Tk()
+root.title("机票价格监测")
+
+# 注册窗口关闭事件
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+# 一些标签
+running_label = tk.Label(root, text="尚未开始监测")
+running_label.pack()
+rounds_label = tk.Label(root, text="")
+rounds_label.pack()
+flight_count_label = tk.Label(root, text="")
+flight_count_label.pack()
+
+# 一些按钮
+start_button = tk.Button(root, text="开始监测", command=start_monitoring)
+start_button.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=10, pady=5)
+
+stop_button = tk.Button(root, text="停止监测", command=stop_monitoring)
+stop_button.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=10, pady=5)
+
+stop_event = threading.Event()
+
+# 启动
+thread = threading.Thread(target=main_thread)
+thread.start()
+root.mainloop()
+thread.join()
